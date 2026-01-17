2c188ea61cdbc309a67b59d556816fe3647f0483
Sabino Hdz <sakuraeyes21@gmail.com>
2026-01-16 19:37:34 -0600

    fix:[home] all field are optional for the results

--- src/types/Patient.ts
+++ src/types/Patient.ts
@@ -6,19 +6,19 @@ export interface Patient {
 }
 
 export interface ResultPatient {
-  imc: number | string
-  healthyWeight: string
-  glucoseLevel: string | number
-  bloodPressure: string
-  cholesterolLevel: string | number
+  imc?: number | string
+  healthyWeight?: string
+  glucoseLevel?: string | number
+  bloodPressure?: string
+  cholesterolLevel?: string | number
 }
 export interface AntropometricData {
-  imc: number | string
-  skinFold: number | string
-  waistCircumference: number | string
+  imc?: number | string
+  skinFold?: number | string
+  waistCircumference?: number | string
 }
 export interface SignalVital {
-  heartRate: string | number
-  bloodPressure: string
-  oxygenSaturation: string | number
+  heartRate?: string | number
+  bloodPressure?: string
+  oxygenSaturation?: string | number
 }
